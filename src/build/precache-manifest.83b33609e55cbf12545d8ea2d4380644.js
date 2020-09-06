0cceed5c21a6e8ea264748bb3e688ffdfedc7fb9
LAPTOP-V7CE2C0D\Nhat Dinh <nguyendinhnhat206@gmail.com>
2020-09-06 15:12:04 -0400

    first commit for mywebblogs-backend

---
+++ src/build/precache-manifest.83b33609e55cbf12545d8ea2d4380644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3577b35b3b091ca0fa0453a0821f36e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd0a8ffad2e418f5abd7",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "f74e613cc367a573bf85",
+    "url": "/static/js/2.b6dddff8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b6dddff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0a8ffad2e418f5abd7",
+    "url": "/static/js/main.c5f2580a.chunk.js"
+  },
+  {
+    "revision": "2c8789034d9ae0b9e1ec",
+    "url": "/static/js/runtime-main.623210e1.js"
+  }
+]);
